67176213aeb4953b051da34cc74d91e51cd39829
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-07-29 22:56:36 +0300

    Message

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -9,6 +9,7 @@ abstract class BaseMessage(
     val isIncoming: Boolean = false,
     val date: Date = Date()
 ) {
+
     abstract fun formatMessage(): String
 
     companion object AbstractFactory {
@@ -18,12 +19,23 @@ abstract class BaseMessage(
             chat: Chat,
             date: Date = Date(),
             type: String = "text",
-            payload: Any?
+            payload: Any?,
+            isIncoming: Boolean = false
         ): BaseMessage {
             lastId++
             return when (type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String)
+                "image" -> ImageMessage(
+                    "$lastId",
+                    from,
+                    chat,
+                    date = date,
+                    image = payload as String,
+                    isIncoming = isIncoming
+                )
+                else -> TextMessage(
+                    "$lastId", from, chat, date = date, text = payload as String,
+                    isIncoming = isIncoming
+                )
             }
         }
     }
