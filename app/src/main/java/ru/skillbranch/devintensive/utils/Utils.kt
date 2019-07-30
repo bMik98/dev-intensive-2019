444903fe446a43ecd7105f9ed468b739231a63e0
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-07-30 19:57:17 +0300

    toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -5,6 +5,30 @@ object Utils {
         val nameParts: List<String>? = fullName?.trim()?.split(" ")
         val firstName = nameParts?.getOrNull(0)
         val lastName = nameParts?.getOrNull(1)
-        return Pair(when(firstName) {"" -> null else -> firstName} , lastName)
+        return Pair(
+            when (firstName) {
+                "" -> null
+                else -> firstName
+            }, lastName
+        )
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        val first = getFirstLetter(firstName)
+        val last = getFirstLetter(lastName)
+        return when {
+            first == null && last == null -> null
+            first == null && last != null -> "$last"
+            first != null && last == null -> "$first"
+            else -> "$first$last"
+        }
+    }
+
+    private fun getFirstLetter(value: String?): Char? {
+        return when {
+            value.isNullOrEmpty() -> null
+            value.isNullOrBlank() -> null
+            else -> value.trim().first().toUpperCase()
+        }
     }
 }
