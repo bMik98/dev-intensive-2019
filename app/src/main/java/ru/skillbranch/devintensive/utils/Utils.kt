c019617f805e2de059d5faa1c72c244c4e9fbd7b
Mikhail Bedritskiy <bmik9898@gmail.com>
2019-07-30 15:15:24 +0300

    parseFullName bug fixed

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,9 +2,9 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val nameParts: List<String>? = fullName?.split(" ")
+        val nameParts: List<String>? = fullName?.trim()?.split(" ")
         val firstName = nameParts?.getOrNull(0)
         val lastName = nameParts?.getOrNull(1)
-        return firstName to lastName
+        return Pair(when(firstName) {"" -> null else -> firstName} , lastName)
     }
 }
